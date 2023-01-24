21717b08e6fdd91dfa114da072eb0dd7bcec36d0
Nifrancy <122533973+Nifrancy@users.noreply.github.com>
2023-01-25 00:18:04 +0300

    Add files via upload

---
+++ area of a circle.c
@@ -0,0 +1,13 @@
+//PROGRAM TO FIND THE AREA OF A CIRCLE//
+#include<stdio.h>
+#define pi=3.142
+int main()
+{
+	float radius,area;
+	printf("ENTER THE RADIUS: ");
+	scanf("%f",&radius);
+	area=3.142*radius*radius;
+	printf("THE AREA;%f",area);
+	
+	return 0;
+}
---
+++ product of two numbers.c
@@ -0,0 +1,14 @@
+//PROGRAM TO FIND PRODUCT OF TWO NUMBERS//
+#include<stdio.h>
+int main()
+{
+	int x,y,prod;
+	printf("ENTER THE VALUE OF x: ");
+	scanf ("%d",&x);
+	printf ("ENTER THE VALUE OF y: ");
+	scanf ("%d",&y);
+	prod=x*y;
+	printf("product=%d,prod");
+	
+	return 0;
+}
